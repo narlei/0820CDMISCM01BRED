95b373ea982ee3e62d69d9aed27a456391132105
Narlei Moreira <narlei.guitar@gmail.com>
2020-09-09 10:36:29 -0300

    Add class property with class type

--- POO2.playground/Contents.swift
+++ POO2.playground/Contents.swift
@@ -1,15 +1,14 @@
 import UIKit
 
-
 class Calculo {
     var numero1: Int?
     var numero2: Int?
-    
+
     func calcula() -> Int {
         if let nro1 = numero1, let numero2 = numero2 {
             return nro1 + numero2
         }
-        
+
         return 0
     }
 }
@@ -19,7 +18,7 @@ class Subtrai: Calculo {
         if let nro1 = numero1, let numero2 = numero2 {
             return nro1 - numero2
         }
-        
+
         return 0
     }
 }
@@ -36,8 +35,6 @@ desconto.numero1 = 20
 desconto.numero2 = 5
 desconto.calcula()
 
-
-
 class Animal {
     var nome: String
     var cor: String
@@ -45,15 +42,14 @@ class Animal {
         self.nome = nome
         self.cor = cor
     }
-    
+
     func comer() { // função void
         print("O animal come")
     }
-    
+
     func emitirSom() -> String {
         return "Som emitido pelo animal"
     }
-    
 }
 
 class Gato: Animal {
@@ -62,19 +58,55 @@ class Gato: Animal {
         let animalSom = super.emitirSom()
         return "\(animalSom) e o gato faz Miauuuuuuuu"
     }
-    
+
     override func comer() {
         super.comer()
         print("O gato Devorou o Whiskas")
     }
 }
 
-
 class Vaca: Animal {
     override func emitirSom() -> String {
         return "Muuuuuuuuuu"
     }
 }
+
+class Fazenda {
+    var nome: String
+    var vaca: Vaca
+    var gato: Gato
+    var outroAnimal: Animal
+    
+    init(vaca: Vaca, gato: Gato, outroAnimal: Animal, nome: String) {
+        self.vaca = vaca
+        self.gato = gato
+        self.outroAnimal = outroAnimal
+        self.nome = nome
+    }
+}
+
+let nomeVaca = "Mimosa"
+let simba = Vaca(nome: nomeVaca, cor: "azul")
+let mingau = Gato(nome: "Mingau", cor: "roxo")
+let frango = Animal(nome: "Frangolino", cor: "lilás")
+
+let fazendaFeliz = Fazenda(vaca: simba, gato: mingau, outroAnimal: frango, nome: "fazendaFeliz")
+
+
+let fazendinha = Fazenda(vaca: simba,
+                         gato: mingau,
+                         outroAnimal: frango,
+                         nome: "fazendinha")
+
+// indentação
+let ypeAmarelo = Fazenda(vaca: Vaca(nome: "Mimosa", cor: "azul"),
+                         gato: Gato(nome: "Mingau", cor: "roxo"),
+                         outroAnimal: Animal(nome: "Frangolino", cor: "lilás"),
+                         nome: "ypeAmarelo")
+
+let mcDonald = Fazenda(vaca: Vaca(nome: "Mimosa", cor: "azul"), gato: Gato(nome: "Mingau", cor: "roxo"), outroAnimal: Animal(nome: "Frangolino", cor: "lilás"), nome: "mcDonald")
+
+
 
 
 let gato = Gato(nome: "Garfield", cor: "Laranja")
@@ -87,5 +119,3 @@ print("\n--------------\n")
 let mimosa = Vaca(nome: "Mimosa", cor: "preta e branca")
 let somVaca = mimosa.emitirSom()
 print(somVaca)
-
-

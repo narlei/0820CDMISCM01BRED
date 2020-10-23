ae3c80b6d9762d7242ed1839182fc860e558f94d
Narlei Moreira <narlei.guitar@gmail.com>
2020-10-23 10:14:47 -0300

    Add Autolayout sample

---
+++ 10_23_Autolayout/10_23_Autolayout/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  10_23_Autolayout
+//
+//  Created by Narlei A Moreira on 23/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+

e65420e0dc7f31e4bac4bae6375029407f59db7a
Narlei Moreira <narlei.guitar@gmail.com>
2020-11-04 12:08:15 -0300

    Cocoapods

---
+++ 11_04_Gerenciador_Bibliotecas/11_04_Gerenciador_Bibliotecas/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  11_04_Gerenciador_Bibliotecas
+//
+//  Created by Narlei A Moreira on 04/11/20.
+//
+
+import UIKit
+import Kingfisher
+
+class ViewController: UIViewController {
+    
+    var arrayAll = [String]()
+    
+    
+    var arrayOpen = [String]()
+    var arrayClose = [String]()
+    
+    
+    
+    @IBOutlet weak var imageViewTest: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://raw.githubusercontent.com/onevcat/Kingfisher/master/images/logo.png")
+        imageViewTest.kf.setImage(with: url)
+        
+        arrayAll = ["Coca cola", "Maçã"]
+        
+        imageViewTest.isHidden = false
+        
+    }
+    
+//    func loadData() {
+//        arrayOpen = [String]()
+//        arrayClose = [String]()
+//    
+//        
+//        var arrayFiltered = [String]()
+//        
+//        if !searchBar.text.isEmpty() {
+//            arrayFiltered = arrayAll.filter({ (item) -> Bool in
+//                return item.contains("xcw")
+//            })
+//        }else {
+//            arrayFiltered.append(contentsOf: arrayAll)
+//        }
+//        
+//        for item in arrayFiltered {
+//            if item.isCompleted {
+//                arrayClose.append(item)
+//            } else {
+//                arrayOpen.append(item)
+//            }
+//        }
+//        
+//        tableView.reloadData()
+//    }
+    
+    
+    
+    
+    
+}

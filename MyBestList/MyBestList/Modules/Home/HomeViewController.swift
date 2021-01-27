199df67766e43978404a133e1819bc41386d9973
Narlei Moreira <narlei.guitar@gmail.com>
2021-01-27 10:04:46 -0300

    Add firebase

--- MyBestList/MyBestList/Modules/Home/HomeViewController.swift
+++ MyBestList/MyBestList/Modules/Home/HomeViewController.swift
@@ -5,19 +5,98 @@
 //  Created by Narlei A Moreira on 14/01/21.
 //
 
+import FirebaseDatabase
 import UIKit
 
 class HomeViewController: BaseViewController {
-
     @IBAction func actionSair(_ sender: Any) {
         UserManager.signOut()
         let loginView: LoginViewController = LoginViewController.getModule()
         UIViewController.replaceRootViewController(viewController: loginView)
+    
     }
+
+    var ref: DatabaseReference!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        ref = Database.database().reference()
+        ref.child("users").child("narlei_123").child("favoritos").child("book_one").setValue(
+            ["name": "Sherlock Holmes"]
+        )
+        
+        ref.child("users").child("narlei_123").child("favoritos").child("book_two").setValue(
+            ["name": "Joaozinho"]
+        )
+        
+        ref.child("users").child("narlei_123").child("favoritos").child("book_zero").setValue(
+            ["name": "Pedrinho"]
+        )
+
+        ref.child("users").child("narlei_123").child("favoritos").observeSingleEvent(of: .value, with: { snapshot in
+            // Get user value
+            if let value = snapshot.value as? NSDictionary{
+                for key in value.allKeys {
+                    if let dicBook = value[key] as? NSDictionary {
+                        print(dicBook)
+                        
+                        
+                    }
+                }
+            }
+
+        }) { error in
+            print(error.localizedDescription)
+        }
     }
-
-
+    
+    
+    func save(object: BaseClassProtocol) {
+        ref = Database.database().reference()
+        ref.child("users").child("narlei_123").child(object.getTableName()).child(object.getId()).setValue(
+            object.toDictionary()
+        )
+    }
 }
 
+class Pessoa: BaseClassProtocol {
+    func getId() -> String {
+        return id!
+    }
+    
+    func getTableName() -> String {
+        return "favoritos"
+    }
+    
+    func toDictionary() -> [String:Any] {
+        return ["id": id!]
+    }
+    
+    var name: String?
+    var id: String?
+}
+
+class Book: BaseClassProtocol {
+    func getId() -> String {
+        return id!
+    }
+    
+    func getTableName() -> String {
+        return "favoritos"
+    }
+    
+    func toDictionary() -> [String:Any] {
+        return ["id": id!]
+    }
+    
+    var name: String?
+    var id: String?
+    
+}
+
+
+protocol BaseClassProtocol {
+    func getId() -> String
+    func getTableName() -> String
+    func toDictionary() -> [String:Any]
+}
--- MyBestList/MyBestList/ViewController.swift
+++ MyBestList/MyBestList/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    var ref: DatabaseReference!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference()
+        ref.child("users").child("123").setValue(["username": "Narlei"])
+        
+        
+        
     }
 
 
